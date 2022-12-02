771741464ededeab2393859aa25bad21aa430e63
amal george manjila <amalgeorge740@gmail.com>
2022-12-02 14:11:54 +0530

    #variable binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,27 +7,70 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
-  aim="Your Perfect Banking Partner";
-  data="Enter account number";
-  acno="";
-  psw="";
-  userDetails:any={
-    1000:{acno:1000,username:"anu",password:123,balance:0},
-    1001:{acno:1001,username:"amal",password:123,balance:0},
-    1002:{acno:1002,username:"arun",password:123,balance:0},
-    1003:{acno:1003,username:"mega",password:123,balance:0}
+  aim = "Your Perfect Banking Partner";
+  data = "Enter account number";
+  acno = "";
+  psw = "";
+  userDetails: any = {
+    1000: { acno: 1000, username: "anu", password: 123, balance: 0 },
+    1001: { acno: 1001, username: "amal", password: 123, balance: 0 },
+    1002: { acno: 1002, username: "arun", password: 123, balance: 0 },
+    1003: { acno: 1003, username: "mega", password: 123, balance: 0 }
   }
 
-login(){
-  alert('login clicked')
-}
+  // login(){
+  //   var acno=this.acno;
+  //   var psw=this.psw;
+  //   var userDetails=this.userDetails;
+  //   if(acno in userDetails){
+  //     if(psw==userDetails[acno]["password"]){
+  //       alert("login successfull")
+  //     }else{
+  //       alert("incorrect password")
+  //     }
+  //   }
+  //  else{
+  //   alert("incorrect username")
+  //  }
+  // }
 
-acnoChange(event:any){
-  this.acno=event.target.value
-}
-pswChange(event:any){
-  
-  
-  this.psw=event.target.value
-}
+
+
+  // login button function
+
+
+
+  login(a:any,b:any) {
+
+    this.acno=a.value;
+    this.psw=b.value;
+    var acno = this.acno;
+    var psw = this.psw;
+    var userDetails = this.userDetails;
+    if (acno in userDetails) {
+      if (psw == userDetails[acno]["password"]) {
+        alert("login successfull")
+      } else {
+        alert("incorrect password")
+      }
+    }
+    else {
+      alert("incorrect username")
+    }
+  }
+
+
+  // ------------------------
+
+
+  // acnoChange(event:any){
+  //   this.acno=event.target.value
+  // }
+  // pswChange(event:any){
+
+
+  //   this.psw=event.target.value
+  // }
+
+
 }
