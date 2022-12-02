e1d6fef67f72cc002c475013e5b15bd6321aff39
amal george manjila <amalgeorge740@gmail.com>
2022-12-02 13:37:03 +0530

    string interpollation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  aim="Your Perfect Banking Partner";
+  data="Enter account number";
+  acno="";
+  psw="";
+  userDetails:any={
+    1000:{acno:1000,username:"anu",password:123,balance:0},
+    1001:{acno:1001,username:"amal",password:123,balance:0},
+    1002:{acno:1002,username:"arun",password:123,balance:0},
+    1003:{acno:1003,username:"mega",password:123,balance:0}
+  }
+
+login(){
+  alert('login clicked')
+}
+
+acnoChange(event:any){
+  this.acno=event.target.value
+}
+pswChange(event:any){
+  
+  
+  this.psw=event.target.value
+}
 }
